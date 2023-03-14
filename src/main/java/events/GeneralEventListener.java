66cdd59952eaa1720b3052f8e126464e9895ba2d
Sinnaminty <aidenrivera7@gmail.com>
2023-03-14 13:28:17 -0400

    made some extra functions learned how slash commands works

--- src/main/java/events/GeneralEventListener.java
+++ src/main/java/events/GeneralEventListener.java
@@ -21,7 +21,6 @@ public class GeneralEventListener extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getContentRaw();
         User user = event.getAuthor();
-        String userString = user.toString();
         MessageChannel channel = event.getChannel();
 
         System.out.println(user.getName() + ": " + content);
@@ -36,18 +35,50 @@ public class GeneralEventListener extends ListenerAdapter {
         System.out.println("Slash command called by " + event.getUser().getName());
         switch(event.getName()) {
             case "parrot":
-                if(parrotList.contains(event.getUser().getId())){
-                    parrotList.remove(event.getUser().getId());
-                    event.reply("No longer parroting " + event.getUser().getName()).queue();
-                }else{
-                    parrotList.add(event.getUser().getId());
-                    event.reply("Squawk! Now parroting " + event.getUser().getName()).queue();
-                }
+                parrotFunc(event);
                 break;
             case "about":
-                event.reply("M1MiC\nborn on 12/12/22\nmade by sin").queue();
+                aboutFunc(event);
+                break;
+            case "ram":
+                ramFunc(event);
+                break;
+            case "bark":
+                barkFunc(event);
                 break;
 
         }
+    }
+    public void parrotFunc(SlashCommandInteractionEvent event){
+        if(parrotList.contains(event.getUser().getId())){
+            parrotList.remove(event.getUser().getId());
+            event.reply("No longer parroting " + event.getUser().getName()).queue();
+        }else{
+            parrotList.add(event.getUser().getId());
+            event.reply("Squawk! Now parroting " + event.getUser().getName()).queue();
+        }
+    }
+    public void aboutFunc(SlashCommandInteractionEvent event){
+        event.reply("M1MiC\nborn on 12/12/22\nmade by sin").queue();
+    }
+    public void ramFunc(SlashCommandInteractionEvent event){
+        try {
+            int ramSticks = event.getOption("ramsticks").getAsInt();
+            User rammd = event.getOption("user").getAsUser();
+            event.reply(String.format("*Eats %d stick(s) of %s's RAM.*", ramSticks, rammd.getName())).queue();
+        }catch(Exception e){
+            System.out.println(e);
+            errorFunc(event);
+        }
+    }
+    public void barkFunc(SlashCommandInteractionEvent event){
+        try {
+            event.reply(event.getOption("message").getAsString()).queue();
+        }catch(Exception e){
+            errorFunc(event);
+        }
+    }
+    public void errorFunc(SlashCommandInteractionEvent event){
+        event.reply("Something went wrong!").queue();
     }
 }
